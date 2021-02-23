a166031c18b9739d69cb3ae609eb5f3bf3b8d4c4
Roman Kohutyak <romankohutyak@gmail.com>
2021-02-23 14:43:47 +0200

    feat: add field container

--- snake-react-game/src/containers/App.js
+++ snake-react-game/src/containers/App.js
@@ -1,5 +1,14 @@
-function App() {
-  return <div className="App"></div>;
+import React, { Component } from 'react';
+import classes from './App.module.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={classes.app}>
+        <div className={classes.app__field}></div>
+      </div>
+    );
+  }
 }
 
 export default App;

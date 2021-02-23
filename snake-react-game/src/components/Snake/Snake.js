1544b2b7da99f21e6ce5fde16da77390e13b6192
Roman Kohutyak <romankohutyak@gmail.com>
2021-02-23 16:03:57 +0200

    feat: add keys listener for snake

--- snake-react-game/src/components/Snake/Snake.js
+++ snake-react-game/src/components/Snake/Snake.js
@@ -4,8 +4,8 @@ import classes from './Snake.module.css';
 const Snake = (props) => {
   return (
     <div>
-      {props.snakeCoordinates.map((cell, index) => {
-        const coordinatesStyles = {
+      {props.snakeStartPosition.map((cell, index) => {
+        const StartPositionStyles = {
           left: `${cell[0]}%`,
           top: `${cell[1]}%`,
         };
@@ -13,7 +13,7 @@ const Snake = (props) => {
           <div
             key={index}
             className={classes.snake__cell}
-            style={coordinatesStyles}
+            style={StartPositionStyles}
           ></div>
         );
       })}
--- snake-react-game/src/containers/App.js
+++ snake-react-game/src/containers/App.js
@@ -4,18 +4,43 @@ import Snake from '../components/Snake/Snake';
 
 class App extends Component {
   state = {
-    snakeCoordinates: [
-      [10, 49],
-      [12, 49],
-      [14, 49],
+    direction: 'TOP',
+    snakeStartPosition: [
+      [49, 77],
+      [49, 75],
+      [49, 73],
     ],
   };
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.keydownHandler.bind(this));
+  }
+
+  keydownHandler(event) {
+    console.log(event.keyCode);
+    switch (event.keyCode) {
+      case 37:
+        this.setState({ direction: 'LEFT' });
+        break;
+      case 38:
+        this.setState({ direction: 'TOP' });
+        break;
+      case 39:
+        this.setState({ direction: 'RIGHT' });
+        break;
+      case 40:
+        this.setState({ direction: 'BOTTOM' });
+        break;
+      default:
+        return;
+    }
+  }
 
   render() {
     return (
       <div className={classes.app}>
         <div className={classes.app__field}>
-          <Snake snakeCoordinates={this.state.snakeCoordinates} />
+          <Snake snakeStartPosition={this.state.snakeStartPosition} />
         </div>
       </div>
     );
